0b8ffe37fdb61bcf85b50c96cc11e4927f6963dd
greg fielding <greg@fieldingdesigngroup.com>
2017-01-29 12:48:12 -0800

    forgot password page

--- js/components/auth/forgotPassword/index.js
+++ js/components/auth/forgotPassword/index.js
@@ -1,9 +1,12 @@
 import React from "react";
 import { connect } from 'react-redux';
-import { TouchableOpacity } from "react-native";
+import { Font } from 'exponent';
+import FontAwesome from 'react-native-vector-icons/FontAwesome';
+import { TouchableOpacity, Image, Alert } from "react-native";
 import { Container, Header, Title, Button, Content, View, Text, Input, InputGroup, Icon } from "native-base";
 import { actions } from 'react-native-navigation-redux-helpers';
 import styles from "./styles";
+import Exponent from 'exponent';
 import { firebaseApp } from "../authentication";
 
 const {
@@ -27,7 +30,8 @@ class ForgotPassword extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      email: ""
+      email: "",
+      fontLoaded: false
     };
   }
 
@@ -44,35 +48,45 @@ class ForgotPassword extends React.Component {
     this.props.popRoute(this.props.navigation.key);
   }
 
+  async componentDidMount() {
+    await Font.loadAsync({
+      'veneer': require('../../../../assets/fonts/veneer.ttf'),
+    });
+    this.setState({ fontLoaded: true });
+  }
+
   render() {
+    if (!this.state.fontLoaded) {return null;}
+    const reset = this.state.fontLoaded ? "Send Reset Email" : null;
     return (
-      <Container style={styles.container}>
-        <Header>
+      <Image source={require('../../../../images/beans2.jpg')} style={styles.container}>
+      <Header style={styles.header}>
           <Button transparent onPress={this.goBack.bind(this)}>
-            <Icon name="ios-arrow-back" style={{ fontSize: 30, lineHeight: 32 }} />
+            <Icon name='ios-arrow-back' style={{color:"#fff"}} />
           </Button>
-          <Title>Reset Password</Title>
+          <Title style={styles.headerText}>Reset Password</Title>
         </Header>
-        <Content>
-          <View style={styles.bg}>
-            <Text style={styles.feedback}>{this.state.result}</Text>
-            <InputGroup style={styles.input}>
-              <Icon name="ios-person" />
-              <Input
-                style={styles.inputValue}
-                placeholder="EMAIL"
-                onChangeText={(text) => {this.setState({ email: text })}} />
-            </InputGroup>
-            <View style={styles.links}>
-              <TouchableOpacity
-                onPress={() => this.changePassword()}
-              >
-                <Text style={styles.link}>Send Reset Email</Text>
-              </TouchableOpacity>
-            </View>
-          </View>
-        </Content>
-      </Container>
+        <View style={styles.logoContainer}>
+        <Image
+          style={styles.logoImage}
+          source={{uri:'https://firebasestorage.googleapis.com/v0/b/coffee-shop-mobile.appspot.com/o/logofritzwhite.png?alt=media&token=1c9f80e9-53ca-42cd-82b9-e6ca72c25d6f'}} />
+        </View>
+        <View style={styles.buttonContainer}>
+          <InputGroup style={styles.input}>
+            <Icon name="ios-person" style={{ color:"white" }} />
+            <Input
+              style={styles.inputValue}
+              placeholder="EMAIL"
+              onChangeText={(text) => {this.setState({ email: text })}} />
+          </InputGroup>
+        </View>
+        <View style={styles.linkContainer}>
+          <Button transparent
+            onPress={() => this.changePassword()}>
+            <Text style={styles.links}>{reset}</Text>
+          </Button>
+        </View>
+        </Image>
     );
   }
 }
